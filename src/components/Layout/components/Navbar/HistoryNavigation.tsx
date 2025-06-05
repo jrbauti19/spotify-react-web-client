7ece0bf94dffe0a78d7536f181a3943b8c41ddb6
Joshua Bautista <jrbauti19@gmail.com>
2025-06-05 13:05:23 -0700

    fix

--- src/components/Layout/components/Navbar/HistoryNavigation.tsx
+++ src/components/Layout/components/Navbar/HistoryNavigation.tsx
@@ -1,10 +1,10 @@
 import { Space } from 'antd';
 
-import NavigationButton from './NavigationButton';
 import ForwardBackwardsButton from './ForwardBackwardsButton';
+import NavigationButton from './NavigationButton';
 
-import { useTranslation } from 'react-i18next';
 import { memo } from 'react';
+import { useTranslation } from 'react-i18next';
 import { FaSpotify } from 'react-icons/fa6';
 
 const HistoryNavigation = memo(() => {
@@ -14,12 +14,12 @@ const HistoryNavigation = memo(() => {
       <NavigationButton
         text={t('Source code')}
         onClick={() => {
-          window.open('https://github.com/francoborrelli/portfolio', '_blank');
+          window.open('https://github.com/jrbauti19', '_blank');
         }}
-        icon={<FaSpotify size={25} fill='white' />}
+        icon={<FaSpotify size={25} fill="white" />}
       />
 
-      <div className='flex flex-row items-center gap-2 h-full'>
+      <div className="flex flex-row items-center gap-2 h-full">
         <ForwardBackwardsButton flip />
         <ForwardBackwardsButton flip={false} />
       </div>
