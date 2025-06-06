24a90d1f9bd985f973b2da5bcdcc56b7979185bd
Joshua Bautista <jrbauti19@gmail.com>
2025-06-06 16:08:52 -0700

    fix icon

--- src/components/Layout/components/Navbar/HistoryNavigation.tsx
+++ src/components/Layout/components/Navbar/HistoryNavigation.tsx
@@ -5,7 +5,7 @@ import NavigationButton from './NavigationButton';
 
 import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
-import { FaSpotify } from 'react-icons/fa6';
+import { FaCode } from 'react-icons/fa6';
 
 const HistoryNavigation = memo(() => {
   const { t } = useTranslation(['navbar']);
@@ -16,7 +16,7 @@ const HistoryNavigation = memo(() => {
         onClick={() => {
           window.open('https://github.com/jrbauti19', '_blank');
         }}
-        icon={<FaSpotify size={25} fill="white" />}
+        icon={<FaCode size={25} fill="white" />}
       />
 
       <div className="flex flex-row items-center gap-2 h-full">
